9f958ec933d9b5b101cd99a2c28ae7e85b6e010f
teamafricorp <73347346+teamafricorp@users.noreply.github.com>
2021-12-06 05:08:39 -0500

    design

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: 'Roboto',
-  primaryColor: Color(0xFFffbd5c),
+  primaryColor: Color(0xFFE53935),
   secondaryHeaderColor: Color(0xFF009f67),
   disabledColor: Color(0xffa2a7ad),
   backgroundColor: Color(0xFF343636),
@@ -10,6 +10,8 @@ ThemeData dark = ThemeData(
   brightness: Brightness.dark,
   hintColor: Color(0xFFbebebe),
   cardColor: Colors.black,
-  colorScheme: ColorScheme.dark(primary: Color(0xFFffbd5c), secondary: Color(0xFFffbd5c)),
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: Color(0xFFffbd5c))),
+  colorScheme: ColorScheme.dark(
+      primary: Color(0xFFE53935), secondary: Color(0xFFE53935)),
+  textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: Color(0xFFE53935))),
 );
